4a4703e75126ced5fcbd15064f44a7e08284bd60
Laughtonkc <66648527+Laughtonkc@users.noreply.github.com>
2021-04-07 21:01:30 -0700

    spacing

--- src/Carousel.js
+++ src/Carousel.js
@@ -5,70 +5,73 @@ import Camtestimony from "./Camtestimony.png";
 import Melissatestimony from "./Melissatestimony.png";
 import Maxtestimony from "./Maxtestimony.png";
 
-
 export default function Carousel() {
-return (
-  <div className="container-fluid">
-    <h1 id="heading">What My Colleagues & Clients Say</h1>
-    <div
-      id="carouselExampleControls"
-      className="carousel slide"
-      data-ride="carousel"
-    >
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img
-            id="carousel"
-            className="d-block w-100"
-            src={Alpatestimony}
-            alt="First slide"
-          />
+  return (
+    <div className="container-fluid">
+      <h1 id="heading">What My Colleagues & Clients Say</h1>
+      <div
+        id="carouselExampleControls"
+        className="carousel slide mb-5"
+        data-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              id="carousel"
+              className="d-block w-100"
+              src={Alpatestimony}
+              alt="First slide"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              id="carousel"
+              className="d-block w-100"
+              src={Camtestimony}
+              alt="Second slide"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              id="carousel"
+              className="d-block w-100"
+              src={Melissatestimony}
+              alt="Third slide"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="d-block w-100"
+              src={Maxtestimony}
+              alt="Third slide"
+            />
+          </div>
         </div>
-        <div className="carousel-item">
-          <img
-            id="carousel"
-            className="d-block w-100"
-            src={Camtestimony}
-            alt="Second slide"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            id="carousel"
-            className="d-block w-100"
-            src={Melissatestimony}
-            alt="Third slide"
-          />
-        </div>
-        <div className="carousel-item">
-          <img className="d-block w-100" src={Maxtestimony} alt="Third slide" />
-        </div>
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleControls"
+          role="button"
+          data-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon bg-dark"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleControls"
+          role="button"
+          data-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon bg-dark"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Next</span>
+        </a>
       </div>
-      <a
-        className="carousel-control-prev"
-        href="#carouselExampleControls"
-        role="button"
-        data-slide="prev"
-      >
-        <span
-          className="carousel-control-prev-icon bg-dark"
-          aria-hidden="true"
-        ></span>
-        <span className="sr-only">Previous</span>
-      </a>
-      <a
-        className="carousel-control-next"
-        href="#carouselExampleControls"
-        role="button"
-        data-slide="next"
-      >
-        <span
-          className="carousel-control-next-icon bg-dark"
-          aria-hidden="true"
-        ></span>
-        <span className="sr-only">Next</span>
-      </a>
     </div>
-  </div>
-);
+  );
 }
